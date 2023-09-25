7f6fbd36824130509ec8b452dd6ad4bc2af88178
Stevensup <buseche@unbosque.edu.co>
2023-09-24 23:35:08 -0500

    proyecto completo

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ function closeModal() {
     const modal = document.getElementById('myModal');
     modal.style.display = 'none';
 }
+
+function submitForm() {
+    // Obtén los valores de los campos del formulario
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Verifica si todos los campos están completos
+    if (name && email && message) {
+        // Construye el mensaje que se mostrará en el SweetAlert
+        const alertMessage = `
+            <strong>Name:</strong> ${name}<br>
+            <strong>Email:</strong> ${email}<br>
+            <strong>Message:</strong> ${message}
+        `;
+
+        // Muestra el SweetAlert con los datos del formulario
+        Swal.fire({
+            title: 'Form Data',
+            html: alertMessage,
+            icon: 'info',
+            confirmButtonText: 'OK'
+        });
+    } else {
+        // Si algún campo está vacío, muestra un mensaje de error
+        Swal.fire({
+            title: 'Error',
+            text: 'Por favor, complete todos los campos del formulario.',
+            icon: 'error',
+        });
+    }
+}
